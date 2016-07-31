eb106248af61fa093b3b597ca2aa51b366c6a9cf
Andrew Harvey <andrew.harvey4@gmail.com>
2016-07-31 16:51:15 +1000

    major refactor
    * gtfs2geojson command line program
    * Use fast-csv with fs.ReadStream support to avoid issues with reading
      large files into Strings.
    * Make assumptions about line order in CSV and use alternate approach to
      reading the sequential line data.

--- README.md
+++ README.md
@@ -1,25 +1,28 @@
 # gtfs2geojson
 
-[![CircleCI](https://circleci.com/gh/tmcw/gtfs2geojson/tree/master.svg?style=svg)](https://circleci.com/gh/tmcw/gtfs2geojson/tree/master)<Paste>
-
 Convert [GTFS](https://developers.google.com/transit/gtfs/?hl=en) data into
 [GeoJSON](http://geojson.org/).
 
     npm install --save gtfs2geojson
 
+## Command Line
+
+    gtfs2geojson [path-to-gtfs-directory]
+
+The `shapes.txt` and `stops.txt` files need to be in the directory you specify, or if none is
+specified the current directory is assumed.
+
+`shapes.geojson` and `stops.geojson` will also be created in this directory.
+
 ## API
 
 
-`lines(gtfs)`
+`lines(gtfs, callback)`
 
-Parse GTFS shapes.txt data given as a string and return a GeoJSON FeatureCollection
-of features with LineString geometries.
+Parse GTFS `shapes.txt` data given as a string or readable stream and invokes callback with
+a GeoJSON FeatureCollection of features with LineString geometries.
 
-`stops(gtfs)`
+`stops(gtfs, callback)`
 
-Parse GTFS stops.txt data given as a string and return a GeoJSON FeatureCollection
-of features with Point geometries.
-
-
-
-
+Parse GTFS `stops.txt` data given as a string or readable stream and invokes callback with
+a GeoJSON FeatureCollection of features with Point geometries.
--- index.js
+++ index.js
@@ -1,76 +1,134 @@
-var parseCSV = require('dsv').csv.parse;
+var fs = require('fs');
+var csv = require("fast-csv");
+var assert = require("assert");
 
 
 var gtfs2geojson = {
-  /**
- * Parse GTFS shapes.txt data given as a string and return a GeoJSON FeatureCollection
- * of features with LineString geometries.
- *
- * @param {string} gtfs csv content of shapes.txt
- * @returns {Object} geojson featurecollection
- */
-  lines: function(gtfs) {
-    var shapes = parseCSV(gtfs).reduce(function(memo, row) {
-      memo[row.shape_id] = (memo[row.shape_id] || []).concat(row);
-      return memo;
-    }, {});
-    return {
-      type: 'FeatureCollection',
-      features: Object.keys(shapes).map(function(id) {
-        return {
-          type: 'Feature',
-          id: id,
-          properties: {
-            shape_id: id
-          },
-          geometry: {
-            type: 'LineString',
-            coordinates: shapes[id].sort(function(a, b) {
-              return +a.shape_pt_sequence - b.shape_pt_sequence;
-            }).map(function(coord) {
-              return [
-                parseFloat(coord.shape_pt_lon),
-                parseFloat(coord.shape_pt_lat)
-              ];
+    /**
+     * Parse GTFS shapes.txt data given as a string or readable stream and return a GeoJSON FeatureCollection
+     * of features with LineString geometries.
+     *
+     * @param {string|fs.ReadStream} gtfs csv or readable stream content of shapes.txt
+     * @param {function} callback callback function with the geojson featurecollection as the first argument
+     */
+    lines: function(gtfs, callback) {
+        var shape_id = null;
+        var coordinates = [];
+        var shape_pt_sequence = null;
+        var shape_ids_seen = {}
+
+        var features = [];
+
+        var parser;
+        if (gtfs instanceof fs.ReadStream) {
+            parser = csv.fromStream(gtfs, {headers: true});
+        }else{
+            parser = csv.fromString(gtfs, {headers: true});
+        }
+
+        parser
+            .on("data", function(data){
+                if (data.shape_id != shape_id) {
+                    // new shape
+                    assert.ok(!(data.shape_id in shape_ids_seen), 'shape_id out of order: ' + data.shape_id);
+                    shape_ids_seen[data.shape_id] = true;
+
+                    // commit the previous shape to features, if this isn't the first call
+                    if (shape_id !== null) {
+                        features.push({
+                            type: 'Feature',
+                            id: shape_id,
+                            properties: null,
+                            geometry: {
+                                type: 'LineString',
+                                coordinates: coordinates
+                            }
+                        });
+                    }
+
+                    // reset shape variables
+                    shape_id = data.shape_id;
+                    shape_pt_sequence = null;
+                    coordinates = [];
+                }
+
+                assert.ok(shape_pt_sequence === null || data.shape_pt_sequence > shape_pt_sequence, 'shape_pt_sequence out of order: shape_id=' + data.shape_id + ' shape_pt_sequence=' + data.shape_pt_sequence);
+                shape_pt_sequence = parseInt(data.shape_pt_sequence);
+
+                coordinates.push([parseFloat(data.shape_pt_lon), parseFloat(data.shape_pt_lat)]);
             })
-          }
-        };
-      })
-    };
-  },
+            .on("end", function(){
+                callback({
+                    type: 'FeatureCollection',
+                    features: features
+                });
+            });
+    },
 
-  /**
- * Parse GTFS stops.txt data given as a string and return a GeoJSON FeatureCollection
- * of features with Point geometries.
- *
- * @param {string} gtfs csv content of stops.txt
- * @returns {Object} geojson featurecollection
- *
- */
+    /**
+     * Parse GTFS stops.txt data given as a string or readable stream and return a GeoJSON FeatureCollection
+     * of features with Point geometries.
+     *
+     * @param {string|fs.ReadStream} gtfs csv or readable stream content of stops.txt
+     * @param {function} callback callback function with the geojson featurecollection as the first argument
+     *
+     */
 
-  stops: function(gtfs) {
-    var stops = parseCSV(gtfs)
-    return {
-      type: 'FeatureCollection',
-      features: Object.keys(stops).map(function(id) {
-        return {
-          type: 'Feature',
-          id: stops[id].stop_id,
-          properties: {
-            stop_id: stops[id].stop_id,
-            stop_name: stops[id].stop_name
-          },
-          geometry: {
-            type: 'Point',
-            coordinates: [
-              parseFloat(stops[id].stop_lon),
-              parseFloat(stops[id].stop_lat)
-            ]
-          }
-        };
-      })
-    };
-  }
-}
+    stops: function(gtfs, callback) {
+        var parser;
+        if (gtfs instanceof fs.ReadStream) {
+            parser = csv.fromStream(gtfs, {headers: true});
+        }else{
+            parser = csv.fromString(gtfs, {headers: true});
+        }
+
+        var stops = [];
+        parser
+            .on("data", function(data){
+                stops.push(data);
+            })
+            .on("end", function(){
+                callback({
+                    type: 'FeatureCollection',
+                    features: Object.keys(stops).map(function(id) {
+                        var feature = {
+                            type: 'Feature',
+                            id: stops[id].stop_id,
+                            properties: {
+                                stop_name: stops[id].stop_name,
+                            },
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [
+                                    parseFloat(stops[id].stop_lon),
+                                    parseFloat(stops[id].stop_lat)
+                                ]
+                            }
+                        };
+                        if ('stop_code' in stops[id]) {
+                            feature.properties.stop_code = stops[id].stop_code;
+                        }
+                        if ('location_type' in stops[id]) {
+                            feature.properties.location_type = stops[id].location_type ? 1 : 0;
+                        }
+                        if ('parent_station' in stops[id]) {
+                            feature.properties.parent_station = stops[id].parent_station;
+                        }
+                        if ('stop_timezone' in stops[id]) {
+                            feature.properties.stop_timezone = stops[id].stop_timezone;
+                        }
+                        if ('wheelchair_boarding' in stops[id]) {
+                            feature.properties.wheelchair_boarding = parseInt(stops[id].wheelchair_boarding || 0);
+                        }
+                        if ('platform_code' in stops[id]) {
+                            feature.properties.platform_code = parseInt(stops[id].platform_code || 0);
+                        }
+
+                        return feature;
+                    })
+                });
+            });
+    }
+};
 
 module.exports = gtfs2geojson;
